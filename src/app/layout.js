c0248d2cb48b7222ca369bc52f286615cd35eb79
David Marom <davidmarom@hotmail.com>
2026-03-03 10:45:02 +0200

    wip

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,24 @@ import "./globals.css";
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-export const metadata = { title: "מיני ישראל", description: "מיני ישראל" };
+export const metadata = {
+  title: "מיני ישראל",
+  description: "מיני ישראל",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon.ico", rel: "shortcut icon" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+    other: [
+      {
+        rel: "manifest",
+        url: "/favicon/site.webmanifest",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
