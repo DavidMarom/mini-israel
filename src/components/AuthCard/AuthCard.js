31edba3e96c6633ef802f1c7e0b425c29c1cfdb8
David Marom <davidmarom@hotmail.com>
2026-03-31 22:04:38 +0300

    wip

--- src/components/AuthCard/AuthCard.js
+++ src/components/AuthCard/AuthCard.js
@@ -45,8 +45,8 @@ export default function AuthCard({ loading, user, displayName, bio, money, inven
         <>
           <button onClick={onLogout} className={styles.logoutBtn} aria-label={he.authLogout}>
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-              <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+              <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+              <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
             </svg>
           </button>
           <div className={styles.userRow}>
@@ -102,21 +102,13 @@ export default function AuthCard({ loading, user, displayName, bio, money, inven
           )}
 
           {!hasFarm && (
-            <button
-              onClick={onBuyFarm}
-              className={styles.farmButton}
-              disabled={buyingFarm || (money ?? 0) < 500}
-            >
+            <button onClick={onBuyFarm} className={styles.farmButton} disabled={buyingFarm || (money ?? 0) < 500}>
               {buyingFarm ? he.authBuyFarmBuying : he.authBuyFarm}
             </button>
           )}
 
           {!isVIP && (
-            <button
-              onClick={onBuyVip}
-              className={styles.vipButton}
-              disabled={!onBuyVip || (money ?? 0) < 1500}
-            >
+            <button onClick={onBuyVip} className={styles.vipButton} disabled={!onBuyVip || (money ?? 0) < 1500}>
               {he.authBuyVip}
             </button>
           )}
